c07df1e1193825d475be0ee231b8988b0e6f60b4
createhb21 <alsghk9701@gmail.com>
2023-05-08 21:34:26 +0900

    ✨ 두 번째 테스트 케이스 (#2)

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -19,4 +19,10 @@ describe("계산기 앱 테스트", () => {
   it("디스플레이에 기본적으로 숫자 0이 표시된다.", () => {
     cy.get("#total").should("have.text", 0);
   });
+
+  // know to unknown, 점진적으로 원하는 결과치에 다가가자
+  it("숫자 버튼을 클릭하면 display에 숫자가 표시된다.", () => {
+    cy.get(".digit").contains("1").click();
+    cy.get("#total").should("have.text", 1);
+  });
 });
---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+function App() {
+  const $digits = document.querySelector(".digits");
+  const $total = document.querySelector("#total");
+
+  const handleDigitClick = (e) => {
+    if (e.target.classList.contains("digit")) {
+      $total.innerText = e.target.innerText;
+    }
+  };
+
+  $digits.addEventListener("click", handleDigitClick);
+}
+
+App();
